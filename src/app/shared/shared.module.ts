7c58a437b3a16dedbf7fe03a19cf4c1b8c34c860
alvaro16589 <the.truth.in.lies.yes@gmail.com>
2022-06-07 21:41:51 -0400

    corrigiendo footer, agregando componente asideBar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,17 +5,20 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { AsideBarComponent } from './components/aside-bar/aside-bar.component';
 
 
 @NgModule({
   declarations: [
     FooterComponent,
     HeaderComponent,
+    AsideBarComponent,
     
   ],
   exports:[
     FooterComponent,
     HeaderComponent,
+    AsideBarComponent,
   ],
   imports: [
     CommonModule,
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+export class mainFun {
+  ruta = "/assets/plugins/jquery/jquery.min.js";
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
